dd88cc72787dac2d1d92d6e27eccebca2d0b70c5
Abdul Rohman <56723300+4bdulRohman@users.noreply.github.com>
2021-10-02 20:31:43 +0700

    Create DoWhile.java

---
+++ DoWhile.java
@@ -0,0 +1,17 @@
+public class PracDoWhile {
+     public static void main(String[] args){
+        int number,fac,i;
+        System.out.println("=====Program Calculate Factorial Values With for======");
+        System.out.println("Enter Number :");
+        Scanner input = new Scanner(System.in);
+        number = input.nextInt();
+        fac = 1;
+        i = 1;
+        do{
+            fac = fac * i;
+            i++;
+        }
+        while(i<=number);
+        System.out.printf("Factorial value of that Number is : %d \n",fac);
+    }
+}
