981a43345bbe740c99461b95b219cdf9d0a1f952
Ashish Kotyan <Ashishkotyan20@gmail.com>
2021-10-03 14:45:21 +0530

    Adding a Clock Function

---
+++ Digital  Clock/Logic.js
@@ -0,0 +1,18 @@
+function updateClock(){
+
+    var now = new Date();
+    var dname = now.getDay(),
+        mo = now.getMonth(),
+        dnum = now.getDate(),
+        yr  = now.getFullYear(),
+        hou = now.getHours(),
+        min = now.getMinutes(),
+        sec = now.getSeconds(),
+        pe = "AM";
+        if(hou==0){
+            hou=12;
+        }
+        if(hou>12){
+            hou = hou - 12;
+            pe = "PM";
+        }
