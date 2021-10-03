7cd2b945abd2a56e4bd99211a557c433e62eb48e
Ashish Kotyan <Ashishkotyan20@gmail.com>
2021-10-03 14:47:04 +0530

    Add Arrays Method for Months

--- Digital  Clock/Logic.js
+++ Digital  Clock/Logic.js
@@ -20,3 +20,8 @@ function updateClock(){
             for(var n = this.toString(); n.length < digit; n = 0 + n );
             return n;
         }
+
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        var weeks = ["Sunday","Monday","tuesday","Wednesday",'Thursday',"Friday","Saturday"];
+        var ids = ["dayname","month","Daynum","year","hour","minute","scond","period"];
+        var values = [weeks[dname],months[mo],dnum.pad(2),yr,hou.pad(2),min.pad(2),sec.pad(2),pe ]
