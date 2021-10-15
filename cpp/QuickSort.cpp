085b2870dd7fc3c4e72b17d31e175018d765bbca
Sentrydeca <63970490+Sentrydeca@users.noreply.github.com>
2021-10-15 17:59:28 +0530

    Implementation of QuickSort

---
+++ cpp/QuickSort.cpp
@@ -0,0 +1,74 @@
+// Implementation of QuickSort in C++
+
+#include <iostream>
+using namespace std;
+
+
+// A utility function to swap elements
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+// This function takes last element as pivot, places the pivot 
+// element at its correct position in sorted array, 
+// and places all smaller (smaller than pivot) to left of pivot 
+// and all greater elements to right of pivot
+int division (int arr[], int low, int high)
+{
+	int pivot = arr[high];
+	int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		// If current element is smaller than the pivot
+		if (arr[j] < pivot)
+		{
+			i++; // increment index of smaller element
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+// The main function that implements QuickSort
+// arr[] --> Array to be sorted,
+// low --> Starting index,
+// high --> Ending index 
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		// div is division index
+        // arr[d] is now at right place
+		int div = division(arr, low, high);
+
+		// Separately sort elements before
+		// partition and after partition
+		quickSort(arr, low, div - 1);
+		quickSort(arr, div + 1, high);
+	}
+}
+
+// Function to print an array
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+// Driver Code(main code)
+int main()
+{
+	int arr[] = {10, 7, 8, 9, 1, 5};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	quickSort(arr, 0, n - 1);
+	cout << "Sorted array: \n";
+	printArray(arr, n);
+	return 0;
+}
