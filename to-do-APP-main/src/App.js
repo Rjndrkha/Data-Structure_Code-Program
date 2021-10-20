05f24f573283808cc6d4bbccfc40698c6e599201
Anisha <786anisha786@gmail.com>
2021-10-20 10:16:35 +0530

    Add files via upload

---
+++ to-do-APP-main/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
