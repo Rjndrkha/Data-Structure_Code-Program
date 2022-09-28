6950e22ff8a19f03ce1a02a1dd07f7a51d71c8bb
dark <dark@kali.Dark>
2022-09-28 12:48:38 +0530

    Calculate the entered visa and final grade average in python

---
+++ PYTHON/calulategrade.py
@@ -0,0 +1,2 @@
+visagrade = input('enter your visa grade : ')
+finalgrade = input('enter your final grade : ')average =(float(visagrade)*0.3)+(float(finalgrade)*0.7)print("average :{0} ".format(average))
