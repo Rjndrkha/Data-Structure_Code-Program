c1d92a9837a3201267d092301c4b6279b425e3c8
Prince Kumar <aiswalprince@gmail.com>
2022-10-01 17:57:06 +0530

    add Account_Creater

--- PYTHON/calulategrade.py
+++
@@ -1,2 +0,0 @@
-visagrade = input('enter your visa grade : ')
-finalgrade = input('enter your final grade : ')average =(float(visagrade)*0.3)+(float(finalgrade)*0.7)print("average :{0} ".format(average))
