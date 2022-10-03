27701751fbb06c5e3bbbb1f40fd9ee201ef08a3e
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-10-03 13:57:40 +0530

    Ai TicTacToe

--- Random_Ai_TicTacToe/app/src/main/java/com/example/randomaitictactoe/MainActivity.kt
+++ Random_Ai_TicTacToe/app/src/main/java/com/example/randomaitictactoe/MainActivity.kt
@@ -22,22 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RandomAITicTacToeTheme {
-        Greeting("Android")
     }
 }
