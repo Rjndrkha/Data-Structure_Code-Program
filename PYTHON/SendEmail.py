6c24ee150c19c1bfbf98d01942d84f031e4de464
Aslam Muhammed <aslamsmuhammed1@gmail.com>
2022-10-04 07:38:23 +0100

    SendEmail.py file

---
+++ PYTHON/SendEmail.py
@@ -0,0 +1,18 @@
+import smtplib
+
+sender = 'from@fromdomain.com'
+receivers = ['to@todomain.com']
+
+message = """From: From Person <from@fromdomain.com>
+To: To Person <to@todomain.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+"""
+
+try:
+   smtpObj = smtplib.SMTP('localhost')
+   smtpObj.sendmail(sender, receivers, message)         
+   print "Successfully sent email"
+except SMTPException:
+   print "Error: unable to send email"
