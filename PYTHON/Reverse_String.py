479005475d50b09e0e5e27db349da0766c712d0b
Akash Rao <rao28271@gmail.com>
2022-10-05 17:42:14 +0530

    Created Reverse_String.py

---
+++ PYTHON/Reverse_String.py
@@ -0,0 +1,9 @@
+string = 'Hello World'
+reverse = ''
+for char in string:
+    reverse = char + reverse
+print(reverse)
+
+# this can also be done in 1 line
+# as shown below, 
+# print(string[::-1])
