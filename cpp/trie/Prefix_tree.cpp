3a70beb450195a51ace89ccdc5afe16501ed4651
Garima <garimajain120403@gmail.com>
2022-10-09 18:10:18 +0530

    Added Code

---
+++ cpp/trie/Prefix_tree.cpp
@@ -0,0 +1,38 @@
+class Trie {
+public:
+    Trie() {}
+
+    void insert(string word) {
+        Trie* node = this;
+        for (char ch : word) {
+            ch -= 'a';
+            if (!node->next[ch]) { node->next[ch] = new Trie(); }
+            node = node->next[ch];
+        }
+        node->isword = true;
+    }
+
+    bool search(string word) {
+        Trie* node = this;
+        for (char ch : word) {
+            ch -= 'a';
+            if (!node->next[ch]) { return false; }
+            node = node->next[ch];
+        }
+        return node->isword;
+    }
+
+    bool startsWith(string prefix) {
+        Trie* node = this;
+        for (char ch : prefix) {
+            ch -= 'a';
+            if (!node->next[ch]) { return false; }
+            node = node->next[ch];
+        }
+        return true;
+    }
+
+private:
+    Trie* next[26] = {};
+    bool isword = false;
+};
