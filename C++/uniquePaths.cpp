1d0124c7547d6d9debe84a32e3088aadd32f82f1
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-10-18 22:21:58 +0530

    Create uniquePaths.cpp

---
+++ C++/uniquePaths.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h> 
+
+int dpUniquePaths(int m, int n, vector<vector<int>> &dp){
+    int i, j;
+    for(i=0;i<m;i++){
+        for(j =0;j<n;j++){
+            if(i==0 && j==0){
+              dp[i][j]=1;
+              continue;
+          }
+            int up=0, left=0;
+            if(i>0)
+            up=dp[i-1][j];
+
+            if(j>0)
+            left=dp[i][j-1];
+
+            dp[i][j]=up+left;
+
+
+        }
+
+
+    }
+    return dp[m-1][n-1];
+
+}
+int uniquePaths(int m, int n) {
+
+    vector<vector<int> > dp(m,vector<int>(n,-1));
+	return dpUniquePaths(m, n, dp);
+}
